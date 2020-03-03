a799ff71266e7c5e31fbcf412c16ea7fb8ad616f
AlvaroSebastian98 <alvaro.sebastian98@gmail.com>
2020-03-02 20:02:45 -0500

    first commit

---
+++ pjConexion2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Usar el espacio de nombre para la conexión a Sql Server
+using System.Data.SqlClient;
+
+namespace pjConexion2
+{
+    public partial class frmClientes : Form
+    {
+
+        public frmClientes()
+        {
+            InitializeComponent();
+        }
+
+        //realizar la cadena de conexion a la base de datos
+        SqlConnection cn = new SqlConnection("Data Source=LAB1507-21\\SQLEXPRESS;Initial Catalog=neptuno;Integrated Security=True");
+
+        public void ListaClientes()
+        {
+            using (SqlDataAdapter Df = new SqlDataAdapter("Select * from clientes", cn))
+            {
+                using (DataSet Da = new DataSet())
+                {
+                    Df.Fill(Da, "Clientes");
+                    dgClientes.DataSource = Da.Tables["Clientes"];
+                    lblTotal.Text = Da.Tables["Clientes"].Rows.Count.ToString();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaClientes();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
